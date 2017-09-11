1e99a8d26a149348c05f11496211f69675c46a1d
Vansh Madan <31353777+vanshmadan@users.noreply.github.com>
2017-09-11 14:08:31 -0700

    Editor with FORMAT  bakckground color chooser

--- Editor.java
+++ Editor.java
@@ -3,76 +3,86 @@ import java.awt.event.*;
 import java.awt.*;
 public class Editor extends JFrame implements ActionListener
 {
-JMenuBar mb;
-JMenu File,Edit,Help;
-JMenuItem Cut,Copy,Paste,SelectAll,CharactersTyped;
-JTextArea t1;
+    JMenuBar mb;
+    JMenu File,Edit,Help,Format;
+    JMenuItem Cut,Copy,Paste,SelectAll,CharactersTyped,BgColor;
+    JTextArea t1;
 
 
-Editor()
-{
- setLayout(null);
-CharactersTyped=new JMenuItem("Characters typed");
-Cut=new JMenuItem("Cut");
-Copy=new JMenuItem("Copy");
-Paste=new JMenuItem("Paste");
-SelectAll=new JMenuItem("Select All");
-CharactersTyped.addActionListener(this);
-Cut.addActionListener(this);
-Copy.addActionListener(this);
-Paste.addActionListener(this);
-SelectAll.addActionListener(this);
+    Editor()
+    {
+        setLayout(null);
+        CharactersTyped=new JMenuItem("Characters typed");
+        Cut=new JMenuItem("Cut");
+        BgColor=new JMenuItem("Background Color");
+        Copy=new JMenuItem("Copy");
+        Paste=new JMenuItem("Paste");
+        SelectAll=new JMenuItem("Select All");
+        CharactersTyped.addActionListener(this);
+        Cut.addActionListener(this);
+        Copy.addActionListener(this);
+        Paste.addActionListener(this);
+        SelectAll.addActionListener(this);
+        BgColor.addActionListener(this);
+        mb=new JMenuBar();
+        File=new JMenu("File");
+        Edit=new JMenu("Edit");
+        Format=new JMenu("Format");
+        Help=new JMenu("Help");
+        File.add("Character typed");
+        Edit.add(Cut);
+            Edit.addSeparator();
+        Edit.add(Copy);
+        Edit.addSeparator();
+        Edit.add(Paste);
+        Edit.addSeparator();
+        Edit.add(SelectAll);
+        Edit.addSeparator();
+        Format.add(BgColor);
+        mb.add(File);
+        mb.add(Edit);
+        mb.add(Format);
+        mb.add(Help);
 
- mb=new JMenuBar();
- File=new JMenu("File");
- Edit=new JMenu("Edit");
- Help=new JMenu("Help");
- File.add("Character typed");
- Edit.add(Cut);
- Edit.add(Copy);
- Edit.add(Paste);
- Edit.add(SelectAll);
- mb.add(File);
- mb.add(Edit);
- mb.add(Help);
-  
-  t1=new JTextArea();
-  
-  
-  t1.setBounds(5,5,360,320);  
-  add(t1);
 
-  add(mb);
-  setJMenuBar(mb);
+        t1=new JTextArea();
 
- 
-  setVisible(true);
- }
- public void actionPerformed(ActionEvent e)
-{
 
- if(e.getSource()==Cut)
- {
- t1.cut();
- }
- if(e.getSource()==Paste)
- {
- t1.paste();
- }
- if(e.getSource()==Copy)
- {
- t1.copy();
- }
- if(e.getSource()==SelectAll)
- {
- t1.selectAll();
- }
-} 
- public static void main(String args[])
-{
-Editor F=new Editor();
-F.setSize(400,400);
-F.show();
+        t1.setBounds(5,5,360,320);
+        add(t1);
+
+        add(mb);
+        setJMenuBar(mb);
+
+
+        setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == Cut) {
+            t1.cut();
+        }
+        if (e.getSource() == Paste) {
+            t1.paste();
+        }
+        if (e.getSource() == Copy) {
+            t1.copy();
+        }
+        if (e.getSource() == SelectAll) {
+            t1.selectAll();
+        }
+        if (e.getSource() == BgColor)
+        {
+            Color c=JColorChooser.showDialog(this,"Choose",Color.CYAN);
+            t1.setBackground(c);
+        }
+    }
+    public static void main(String args[])
+    {
+        Editor F=new Editor();
+        F.setSize(400,400);
+        F.setVisible(true);
+    }
 }
-} 
- 
+
+
